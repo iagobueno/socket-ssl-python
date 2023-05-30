546736ab967f31e6a66b6ca33dc6feb15268f679
Iago Bueno <iagobueno.c@gmail.com>
2023-05-29 22:05:39 -0300

    feat: basic setup. Client is sending messages to server.

--- client.py
+++ client.py
@@ -2,15 +2,17 @@ import socket
 import ssl
 
 # Cria um contexto SSL
-context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-context.ssl_version = ssl.PROTOCOL_TLSv1_2
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+context.load_cert_chain(certfile="client.crt", keyfile="client.key")
+context.check_hostname = False
+context.verify_mode = ssl.CERT_NONE
 
 # Cria um socket TCP/IP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     # Víncula o client_socket com o SSL
     with context.wrap_socket(client_socket, server_hostname='localhost') as ssl_client_socket:
         # Conecta ao servidor
-        ssl_client_socket.connect(('localhost', 35412))
+        ssl_client_socket.connect(('localhost', 35423))
 
         # Envia dados ao servidor
         message = "Hello from the client!"
--- server.py
+++ server.py
@@ -2,28 +2,30 @@ import socket
 import ssl
 
 # Cria um contexto SSL
-context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.ssl_version = ssl.PROTOCOL_TLSv1_2
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile="server.crt", keyfile="server.key")
+context.check_hostname = False
+context.verify_mode = ssl.CERT_NONE
 
 # Cria um socket TCP/IP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    server_socket.bind(('localhost', 35412))
-    server_socket.listen(1)
+    server_socket.bind(('localhost', 35423))
+    server_socket.listen(5)
 
     # Víncula o server_socker com o SSL
     with context.wrap_socket(server_socket, server_side=True) as ssl_server_socket:
         print("Server is listening...")
 
         while True:
-            # Aguarda conexão
-            with ssl_server_socket.accept() as client_socket:
-                client_address = client_socket.getpeername()
-                print("Client connected:", client_address)
+        # Aguarda conexão
+            client_socket,client_address = ssl_server_socket.accept()
+            print("Client connected:", client_address)
 
-                # Recebe os dados do cliente
-                data = client_socket.recv(1024)
-                print("Received data:", data.decode())
+            # Recebe os dados do cliente
+            data = client_socket.recv(1024)
+            print("Received data:", data.decode())
 
-                # Envia resposta de volta
-                response = "Hello from the server!"
-                client_socket.send(response.encode())
+            # Envia resposta de volta
+            response = "Hello from the server!"
+            client_socket.send(response.encode())
+            client_socket.close()
